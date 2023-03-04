d9575f755b82dde8ebab6564a98f140918cc9b3e
Hassan-Bin-Tariq <58106042+Ibnet-pixel@users.noreply.github.com>
2023-03-04 16:56:34 +0500

    number of faces check

--- FaceDetectFile.py
+++ FaceDetectFile.py
@@ -43,34 +43,17 @@ def index():
 
     # print(load_image)
     target_image = fr.load_image_file(load_image)
+
     # target_encoding = fr.face_encodings(target_image)
 
     face_location = fr.face_locations(target_image)
 
-    # print(type(target_encoding))
-
-    def create_frame(location, label):
-        top, right, bottom, left = location
-
-        cv.rectangle(target_image, (left, top),
-                     (right, bottom), (255, 0, 0), 2)
-        cv.rectangle(target_image, (left, bottom + 20),
-                     (right, bottom), (255, 0, 0), cv.FILLED)
-        cv.putText(target_image, label, (left + 3, bottom + 14),
-                   cv.FONT_HERSHEY_DUPLEX, 0.4, (255, 255, 255), 1)
-
-    if face_location:
-
-        for location in face_location:
-            label = "Selected File"
-            create_frame(location, label)
-
-    def render_image():
-        rgb_img = cv.cvtColor(target_image, cv.COLOR_RGB2BGRA)
-        cv.imshow('Face Recognition', rgb_img)
-        cv.waitKey(0)
-
-    render_image()
+    if face_location == 1:
+        print("face found")
+    elif (len(face_location) > 1):
+        print("more then one face")
+    else:
+        print("No face")
 
     with open(load_image, "rb") as img_file:
         my_string = base64.b64encode(img_file.read())
